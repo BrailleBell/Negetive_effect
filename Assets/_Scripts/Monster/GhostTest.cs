2851e90b4eb8e4574fca17858a6691bb14228ded
Nicolay Joahsen <77834534+Nicrus@users.noreply.github.com>
2022-01-21 15:55:48 +0100

    Test scene and ghost mechanic

---
+++ Assets/_Scripts/Monster/GhostTest.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GhostTest : MonoBehaviour
+{
+    
+    private GameObject Player;
+    public float ghostSpeed;
+    private Vector3 targetPos;
+    private GameObject Ghost;
+    private Rigidbody rb;
+    private int dir;
+    private bool seen, seenafterTeleport;
+    public float timer;
+    private GameObject[] cover;
+    public GameObject coverTarget;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Ghost = gameObject;
+        rb = GetComponent<Rigidbody>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // behaviour 
+        targetPos = Player.transform.position;
+        Ghost.transform.position =
+            Vector3.MoveTowards(gameObject.transform.position, targetPos, ghostSpeed * Time.deltaTime);
+        
+        gameObject.transform.LookAt(Player.transform.position);
+
+        
+
+
+        if (GetComponent<Renderer>().isVisible)
+        {
+            seen = true;
+            Debug.Log("SYNLIG");
+            
+        }
+        else
+        {
+            seen = false;
+            Debug.Log("IKKE SYNLIG");
+            timer = 0;
+            targetPos = Player.transform.position;
+            Ghost.transform.position =
+                Vector3.MoveTowards(gameObject.transform.position, targetPos, ghostSpeed * Time.deltaTime);
+        
+            gameObject.transform.LookAt(Player.transform.position);
+
+            
+            
+        }
+
+        if (seen)
+        {
+            timer += Time.deltaTime;
+            if (timer > 1.5f)
+            {
+                teleportaway();
+                
+
+            }
+
+            seen = false;
+
+        }
+        
+
+    }
+
+    
+    void teleportaway()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Cover");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+
+        transform.position =  Vector3.MoveTowards(transform.position,closest.transform.position, 100f * Time.deltaTime);
+        Debug.Log("Teleport");
+
+        if (transform.position == closest.transform.position)
+        {
+           // if (timer > 3f)
+           // {
+           //     transform.position = Vector3.MoveTowards(transform.position, targetPos, 10f * Time.deltaTime);
+           // }
+        }
+
+    } 
+
+}
+
