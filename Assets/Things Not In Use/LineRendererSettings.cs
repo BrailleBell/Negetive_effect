70205f82798f5c3e5f828954264b3efb5beca476
Charlie <eikascharlie@gmail.com>
2022-01-27 13:10:58 +0100

    Made test terrain

--- Assets/Things Not In Use/LineRendererSettings.cs
+++ Assets/Things Not In Use/LineRendererSettings.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -81,4 +81,4 @@ public class LineRendererSettings : MonoBehaviour
             }
         }
     }
-}
+}*/
