b1650c4fd89f855adfe551690b3698f8b8833ffe
Charlie <eikascharlie@gmail.com>
2022-02-01 15:52:12 +0100

    menus

---
+++ Assets/_Scripts/Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
