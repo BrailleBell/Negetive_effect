ae6f871aa7da43bcedf9591559f31154957b9d2d
Charlie <77836344+TheKingChu@users.noreply.github.com>
2022-02-23 21:21:32 +0100

    Fixed some stuff + some save stuff

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -9,14 +9,21 @@ public class GameManager : MonoBehaviour
     public GameObject VRheadset;
 
     public GameObject[] spawnPoints;
+
+    //saving stuff
+    public float Timer = 0;
+    public float TimeCheck = 180; //180 = 3 minutes
+    public bool SaveGame = false;
+
     // Start is called before the first frame update
     private void Awake()
     {
         
     }
     void Start()
-    { 
-        
+    {
+        LoadGameFunction();
+
        DontDestroyOnLoad(this); 
        // DontDestroyOnLoad(VRheadset);
        if(GameObject.Find("__GM").activeInHierarchy == true)
@@ -30,7 +37,35 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        
+        Timer = Timer + 1 * Time.deltaTime;
+
+        if(Timer >= TimeCheck)
+        {
+            SaveGame = true;
+        }
+
+        //need to find out how to save the time and makes sure it stays at that time
+        if(SaveGame == true)
+        {
+            SaveGameFunction();
+            LoadGameFunction();
+            Timer = 0; //resets the timer
+            SaveGame = false;
+            Debug.Log(SaveGame);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SaveGameFunction()
+    {
+        PlayerPrefs.SetInt("Films", film);
+    }
+
+    public void LoadGameFunction()
+    {
+        film = PlayerPrefs.GetInt("Films");
     }
 
     // films
