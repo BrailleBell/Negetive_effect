92c5589c24c40b5c9a79bc6f8b75cf9e3d2c2a10
Nicolay <ninicolay@hotmail.com>
2022-02-23 21:38:01 +0100

    Trundle Scripts

--- Assets/_Scripts/Monster/Trundle.cs
+++ Assets/_Scripts/Monster/Trundle.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections;
+using TMPro.EditorUtilities;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
@@ -13,6 +14,7 @@ using UnityEngine.Timeline;
 
 public class Trundle : MonoBehaviour
 {
+    
     // floats && ints
     public float distanceToPlayer;
     private float lerpStuff, lookAngle;
@@ -24,26 +26,31 @@ public class Trundle : MonoBehaviour
     private float groundlevel, baseoffset, attackDist;
     public int GoToSceneWhenKilled;
     private float vectorAngle;
-    
-    
+    public int wayPointInd;
+    public float radiusToWaypoint;
+    public float raduiusAroundTarget,spinningRadius; // Spinningradius always lover than radiusAroundTarget
+    public float circlingSpeed;
+    private float pos;
+    public float _speed;
+
     // States and enums
     public enum State
     {
         Idle,
-        Walking,
+        Patroling,
         Attacking,
-        Underground,
-        AboweGround
+        Chase
     }
 
     public State state;
     
     
     // bools && triggers
-    public bool aboveGround, notTestKill, seesPlayer;
-    private bool shouldLerp, lerpHasStarted, ghostDying, walking;
+    public bool aboveGround, notTestKill, seesPlayer, testOn, circlingThePlayer;
+    private bool shouldLerp, lerpHasStarted, ghostDying, walking, belowGround;
 
     // rest
+    public GameObject[] MonsterWaypoints;
     private GameObject Player;
     private NavMeshAgent ghost;
     private Rigidbody rb;
@@ -52,31 +59,33 @@ public class Trundle : MonoBehaviour
     private AnimationTrack AdjustSpeed;
     public Light chestLight;
     private Color redlight, greenlight;
-
-
+    
+    
+    
     void Start()
     {
+        state = State.Patroling;
         anim = GetComponentInChildren<Animator>();
         ghost = GetComponent<NavMeshAgent>();
         Player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
-        
         redlight = new Color(94, 14, 0, 255); 
         greenlight = new Color(16, 60, 13, 255);
-        state = State.Idle;
+        wayPointInd = Random.Range(1, MonsterWaypoints.Length);
+
+
+
     }
 
   
     void Update()
     {
          distanceToPlayer = Vector3.Distance(transform.position, Player.transform.position);
-         ghost.SetDestination(Player.transform.position);
-         if (distanceToPlayer > awareRadius)
-         {
-             anim.SetTrigger("Down");
-         }
          
-         
+
+
+
+
          switch (state)
         {
             case State.Idle:
@@ -84,16 +93,12 @@ public class Trundle : MonoBehaviour
                 break;
             case State.Attacking:
                 break;
-            case State.Walking:
-                
+            case State.Chase:
+                Chase();
+                break;
+            case State.Patroling:
+                Patroling();
                 LookingForPlayer();
-                anim.SetTrigger("Walk");
-                break;
-            case State.AboweGround:
-                AboveGround();
-                break;
-            case State.Underground:
-                BelowGround();
                 break;
         }
         
@@ -130,14 +135,9 @@ public class Trundle : MonoBehaviour
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
             
-            if ((distanceToPlayer <= LookRange) && (distanceToPlayer >= awareRadius))
+            if ((distanceToPlayer <= LookRange) && (distanceToPlayer <= awareRadius))
             {
-
-
-            }
-            else if ((distanceToPlayer <= LookRange) && (distanceToPlayer <= awareRadius))
-            {
-                AboveGround();
+             
                 if (distanceToPlayer <= ghost.stoppingDistance)
                 {
                     GetComponent<BoxCollider>().enabled = true;
@@ -183,8 +183,6 @@ public class Trundle : MonoBehaviour
             
         }
         
-        
-
 
         if (ghostDying) // after taking picture of the ghost it dies after killtimer 
         {
@@ -223,11 +221,12 @@ public class Trundle : MonoBehaviour
         return Vector3.Lerp(Start, End, lerpLocation);
     }
 
-
+    
+    
     public void AboveGround()
     {
        // anim.ResetTrigger("Down");
-        anim.SetTrigger("Up");
+        
     }
 
     public void BelowGround()
@@ -238,6 +237,35 @@ public class Trundle : MonoBehaviour
         
     }
 
+    public void Patroling()
+    {
+        anim.SetBool("Up",true);
+        anim.SetBool("Walk",true);
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+        if (Vector3.Distance(gameObject.transform.position, MonsterWaypoints[wayPointInd].transform.position) >= radiusToWaypoint)
+        {
+            ghost.SetDestination(MonsterWaypoints[wayPointInd].transform.position);
+            transform.LookAt(MonsterWaypoints[wayPointInd].transform.position);
+            
+        }
+        else if(Vector3.Distance(gameObject.transform.position, MonsterWaypoints[wayPointInd].transform.position) <= radiusToWaypoint)
+        {
+            wayPointInd +=1 ;
+            if (wayPointInd >= MonsterWaypoints.Length - 1)
+            {
+                wayPointInd = 0;
+            }
+        }
+
+        if (seesPlayer)
+        {
+            state = State.Chase;
+        }
+        
+
+
+    }
+
     public void LookingForPlayer()
     {
         Vector2 angleToPlayer = (new Vector2(Player.transform.position.x, Player.transform.position.z) -
@@ -246,21 +274,108 @@ public class Trundle : MonoBehaviour
         if (distanceToPlayer <= LookRange && vectorAngle <= FOV / 2)
         {
             seesPlayer = true;
-            if (distanceToPlayer > awareRadius)
-            {
-                state = State.Underground;
-                ghost.SetDestination(Player.transform.position);
-            }
-            else if(distanceToPlayer < awareRadius)
-            {
-                state = State.AboweGround;
-                ghost.SetDestination(Player.transform.position);
-            }
         }
         else
         {
             seesPlayer = false;
         }
+        
+    }
+
+    public void Chase()
+    {
+        belowGround = true;
+        if (belowGround)
+        {
+            anim.SetBool("Down",true);
+            belowGround = false;
+
+        }
+        else
+        {
+            anim.SetBool("Down",false);
+        }
+        anim.SetBool("Walk",false);
+        anim.SetBool("Up",false);
+        
+        belowTimer += Time.deltaTime;
+        if (belowTimer >= Random.Range(10, 50))
+        {
+           // state = State.Attacking;
+           // belowTimer = 0;
+        }
+
+        ghost.speed = distanceToPlayer / 3;
+        if (distanceToPlayer < raduiusAroundTarget)
+        {
+            float startAngle;
+            if (!circlingThePlayer)
+            {
+                
+                startAngle = Vector3.Angle(transform.position, Player.transform.position);
+                ghost.speed = circlingSpeed;
+                float x = Mathf.Cos(startAngle) * spinningRadius;
+                float y = Mathf.Sin(startAngle) * spinningRadius;
+                Vector3 targetPos = new Vector3(x, 0, y);
+                targetPos = Player.transform.position + targetPos;
+                ghost.SetDestination(targetPos);
+                circlingThePlayer = true;
+                transform.LookAt(targetPos);
+            }
+            else
+            {
+                ghost.acceleration = 100;
+                ghost.angularSpeed = 100;
+                pos += Time.deltaTime * circlingSpeed;
+                ghost.speed = _speed;
+                float angle = pos * 3.14159f;
+                float x = Mathf.Cos(angle) * spinningRadius;
+                float y = Mathf.Sin(angle) * spinningRadius;
+                Vector3 targetPos = new Vector3(x, 0, y);
+                targetPos = Player.transform.position + targetPos;
+                ghost.SetDestination(targetPos);
+                transform.LookAt(targetPos);
+                _speed = circlingSpeed * 50;
+
+
+
+            }
+            
+            
+
+        }
+        else
+        {
+            circlingThePlayer = false;
+            ghost.SetDestination(Player.transform.position);
+        }
+       
+        
+        
+        if (!seesPlayer && !testOn)
+        {
+            state = State.Patroling;
+        }
+        
+    }
+
+    public void Attacking()
+    {
+        if (!aboveGround)
+        {
+            anim.SetBool("Up",true);
+            aboveGround = true;
+        }
+        
+
+        ghost.SetDestination(Player.transform.position);
+        if (distanceToPlayer < 1)
+        {
+            
+            
+            
+        }
+
     }
     
     
@@ -276,6 +391,15 @@ public class Trundle : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + (new Vector3(debugger.x,0,debugger.y)) * LookRange);
         debugger = Quaternion.Euler(0, 0, FOV) * debugger;
         Debug.DrawLine(transform.position, transform.position + (new Vector3(debugger.x,0,debugger.y)) * LookRange);
+        if (ghost != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(ghost.destination,raduiusAroundTarget);
+            Gizmos.DrawSphere(ghost.destination, 1);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(Player.transform.position,spinningRadius);
+        }
+        
     }
     #endregion
    
@@ -286,7 +410,6 @@ public class Trundle : MonoBehaviour
         {
             Debug.Log("monster hit");
             ghostDying = true;
-
 
         }
     }
