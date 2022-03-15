81e6a37be55dac31049a2a28c3f3af706070e5bf
Charlie <77836344+TheKingChu@users.noreply.github.com>
2022-03-15 16:53:33 +0100

    test

--- Assets/_Scripts/Class/Enemy.cs
+++ Assets/_Scripts/Class/Enemy.cs
@@ -2,6 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Empty enemy if we want to just make them automatically
+/// through script instead of having to put stuff manually in prefabs
+/// </summary>
+public class Enemy<T> where T : Enemy
+{
+    public GameObject GameObject;
+    public T ScriptComponent;
+
+    public Enemy(string name)
+    {
+        GameObject = new GameObject(name);
+        ScriptComponent = GameObject.AddComponent<T>();
+    }
+}
+
+/// <summary>
+/// Base script for the enemies
+/// </summary>
 public abstract class Enemy : MonoBehaviour
 {
     public GameObject[] spawnPoints;
