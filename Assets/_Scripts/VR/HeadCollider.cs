cca8d257f0eaad73e1fb6340690add418fafab2f
Charlie <77836344+TheKingChu@users.noreply.github.com>
2022-03-25 12:51:35 +0100

    collider test nr 474380932

--- Assets/_Scripts/VR/HeadCollider.cs
+++ Assets/_Scripts/VR/HeadCollider.cs
@@ -5,18 +5,22 @@ using UnityEngine.XR;
 
 public class HeadCollider : MonoBehaviour
 {
-    public GameObject Camera;
-    public CharacterController characterController;
+    public Camera camera; //this is supposed to be the camera for eyes
+    CapsuleCollider collider;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        collider = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void Update()
     {
-        Camera.transform.position = characterController.center;
+        /*So this will take the position from the center of the camera (aka the eyes) and
+         * subtract the position on the playercontroller
+         * which means that when we move around it gets the position
+         * of the camera relative to the playercontroller*/
+        Vector3 temp = camera.transform.position - this.transform.position;
+        temp.y = collider.transform.position.y;
+        collider.center = temp;
     }
 }
