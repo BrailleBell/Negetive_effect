dd0b215779d9cef40fca3b71245d060e1688dfb1
Nicolay <ninicolay@hotmail.com>
2022-04-27 14:50:08 +0200

    Fmod and skelly boy tests

--- Assets/_Scripts/Monster/SkellyBoi.cs
+++ Assets/_Scripts/Monster/SkellyBoi.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class SkellyBoi : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject Monster;
+    public int monstersAroundPlayer;
+
+    private readonly float AppearWaitDuration;
+    private Transform SurrounderParentTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SurrounderParentTransform = new GameObject(gameObject.name + "Surrounder Parent").transform;
+        Player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(SurroundStepAnimated());
     }
 
-    // Update is called once per frame
+    IEnumerator SurroundStepAnimated()
+    {
+        float AngleStep = 360.0f / monstersAroundPlayer;
+        Player.transform.SetParent(SurrounderParentTransform);
+
+        for (int i = 1; i < monstersAroundPlayer; i++)
+        {
+            GameObject newSurroundMonster = Instantiate(Monster);
+            newSurroundMonster.transform.RotateAround(Player.transform.position,Vector3.up,AngleStep +i);
+            newSurroundMonster.transform.SetParent(SurrounderParentTransform);
+            yield return new WaitForSeconds(AppearWaitDuration);
+            
+        }
+    }
+
+
+// Update is called once per frame
     void Update()
     {
         
