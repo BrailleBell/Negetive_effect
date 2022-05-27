6457cd44fe80fe6f7c0650241d442240302398f9
Nicolay <ninicolay@hotmail.com>
2022-05-27 13:48:14 +0200

    Update PlayerManager.cs

--- Assets/_Scripts/Player/PlayerManager.cs
+++ Assets/_Scripts/Player/PlayerManager.cs
@@ -12,29 +12,44 @@ public class PlayerManager : MonoBehaviour
     public float respawnTimer;
     private float respawnTimerCounter;
     private bool DeathTimer;
+    private Animator anim;
+    private GameObject deathPP;
 
 
     private void Awake()
     { 
         GameObject.Find("XR Origin").transform.position = lastPostPos;
+        deathPP = GameObject.Find("post_death");
     }
 
     void Start()
     {
         Player = GameObject.Find("XR Origin");
+        anim = deathPP.GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DeathTimer)
+        Debug.Log("DeathTimerBool is " + DeathTimer);
+        if (DeathTimer) // A bool, checks if the player has died
         {
             respawnTimerCounter += Time.deltaTime;
             if (respawnTimerCounter > respawnTimer)
             {
+                respawnTimerCounter = 0;
+                //Write here, everything that should happen before death
+                
+                
+                
+                
                 Dying();
             }
+        }
+        else
+        {
+            anim.SetBool("Dead", false);
         }
     }
 
@@ -42,6 +57,7 @@ public class PlayerManager : MonoBehaviour
     {
         if (other.CompareTag("Monster"))
         {
+            anim.SetBool("Dead",true);
             DeathTimer = true;
             Debug.Log("DeathCounter " + respawnTimerCounter);
         }
@@ -52,16 +68,38 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
+    //  private void OnTriggerExit(Collider other)
+    //  {
+    //      if (other.CompareTag("Monster"))
+    //      {
+    //          Dying();
+    //          //DeathTimer = true;
+    //          //Debug.Log("DeathCounter " + respawnTimerCounter);
+    //          
+    //      }
+    //      
+    //      if (other.CompareTag("DeathBarrier"))
+    //      {
+    //          DeathTimer = true;
+    //          Debug.Log("DeathCounter " + respawnTimerCounter);
+    //          
+    //      }
+    //      
+    //      
+    //  }
+
 
     public void Dying()
     {
+        // Write everything that happens during death
         transform.position = lastPostPos;
         // SceneManager.LoadScene(SceneToGoTo);
         
         
         
+        
+        //This is last entry
         DeathTimer = false;
-        respawnTimerCounter = 0;
 
     }
 }
