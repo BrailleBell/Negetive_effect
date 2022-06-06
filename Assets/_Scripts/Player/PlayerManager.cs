af77baf2623b5c33f6a163eee4b686b1a197f907
Charlie <eikascharlie@gmail.com>
2022-06-06 16:11:04 +0200

    Update PlayerManager.cs

--- Assets/_Scripts/Player/PlayerManager.cs
+++ Assets/_Scripts/Player/PlayerManager.cs
@@ -128,9 +128,37 @@ public class PlayerManager : MonoBehaviour
 
         // The death pp transform
         cS.GetComponent<TimeSwap>().Show_again(Player.transform);
-
-        currentTime = 00;
-      //  watch.OnSceneLoaded(SceneManager.GetSceneByBuildIndex(2), LoadSceneMode.Single);
+        #region reset timer
+        if (currentTime < 0 && currentTime > 1)
+        {
+            currentTime = 00;
+        }
+        else if (currentTime < 1 && currentTime > 2)
+        {
+            currentTime = 01;
+        }
+        else if (currentTime < 2 && currentTime > 3)
+        {
+            currentTime = 02;
+        }
+        else if (currentTime < 3 && currentTime > 4)
+        {
+            currentTime = 03;
+        }
+        else if (currentTime < 4 && currentTime > 5)
+        {
+            currentTime = 04;
+        }
+        else if (currentTime < 5 && currentTime > 6)
+        {
+            currentTime = 05;
+        }
+        else
+        {
+            Debug.Log("no hour reached");
+        }
+        #endregion
+        //  watch.OnSceneLoaded(SceneManager.GetSceneByBuildIndex(2), LoadSceneMode.Single);
 
         PlayerDied = true;
             //This is last entry
