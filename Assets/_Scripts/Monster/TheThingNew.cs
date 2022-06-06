0d121625e807858022a24238ca2c4c13ee6b1307
Charlie <eikascharlie@gmail.com>
2022-06-06 14:18:13 +0200

    load button 5

--- Assets/_Scripts/Monster/TheThingNew.cs
+++ Assets/_Scripts/Monster/TheThingNew.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
+//using System.Diagnostics.Eventing.Reader;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEditor;
